5fe2de1a3f432b6317670aed8390f1c2e22aa422
Xcution <xcution.js@gmail.com>
2024-07-28 15:10:56 +0700

    написал функцию обработки операций и функцию вычисления

--- index.js
+++ index.js
@@ -17,6 +17,44 @@ document.addEventListener('DOMContentLoaded', () => {
         updateScreen()
     }
 
+    const handleOperator = (operation) => {
+        if (operator && previousInput && currentInput) {
+            calculate();
+        }
+        operator = operation;
+        previousInput = currentInput;
+        currentInput = '0';
+    }
+
+    const calculate = () => {
+        let result;
+        const previous = parseFloat(previousInput);
+        const current = parseFloat(currentInput);
+
+        if (isNaN(previous) || isNaN(current)) return;
+
+        switch (operator) {
+            case '+':
+                result = previous + current;
+                break;
+            case '-':
+                result = previous - current;
+                break;
+            case 'x':
+                result = previous * current;
+                break;
+            case '/':
+                result = previous / current;
+                break;
+            default:
+                return;
+        }
+
+        currentInput = result.toString();
+        operator = '';
+        previousInput = '';
+    }
+
     document.querySelectorAll('.calc-btn').forEach(button => {
         button.addEventListener('click', () => {
             if (button.classList.contains('nine') || button.classList.contains('eight') ||
@@ -28,9 +66,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 handleNumber(button.textContent);
             } else if (button.classList.contains('plus') || button.classList.contains('minus') ||
                 button.classList.contains('multiply') || button.classList.contains('division')) {
-
-            } else {
-
+                handleOperator(button.textContent);
+            } else if (button.classList.contains('equal')) {
+                calculate();
+                updateScreen();
             }
         });
     });
