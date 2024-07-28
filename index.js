17fcdbd253b3b7b70f3f3ec845873b18f91ee8c7
Xcution <xcution.js@gmail.com>
2024-07-28 15:06:34 +0700

    написал функцию обработки нажатия цифр и обработчик события для кнопок

--- index.js
+++ index.js
@@ -8,5 +8,32 @@ document.addEventListener('DOMContentLoaded', () => {
         screen.textContent = currentInput;
     }
 
+    const handleNumber = (number) => {
+        if (currentInput === '0' && number !== '.') {
+            currentInput = number;
+        } else {
+            currentInput += number;
+        }
+        updateScreen()
+    }
+
+    document.querySelectorAll('.calc-btn').forEach(button => {
+        button.addEventListener('click', () => {
+            if (button.classList.contains('nine') || button.classList.contains('eight') ||
+                button.classList.contains('seven') || button.classList.contains('six') ||
+                button.classList.contains('five') || button.classList.contains('four') ||
+                button.classList.contains('three') || button.classList.contains('two') ||
+                button.classList.contains('one') || button.classList.contains('zero') ||
+                button.classList.contains('dot')) {
+                handleNumber(button.textContent);
+            } else if (button.classList.contains('plus') || button.classList.contains('minus') ||
+                button.classList.contains('multiply') || button.classList.contains('division')) {
+
+            } else {
+
+            }
+        });
+    });
+
     updateScreen();
 })
